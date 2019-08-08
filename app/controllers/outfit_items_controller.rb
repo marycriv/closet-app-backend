af8518cd962cfad6db14cf8a0c9f445607eac4e3
marycriv <maryecrivelli@gmail.com>
2019-08-08 10:57:21 -0400

    Added outfit and join table controllers

---
+++ app/controllers/outfit_items_controller.rb
@@ -0,0 +1,37 @@
+class OutfitItemsController < ApplicationController
+
+  def index
+    @outfit_items = OutfitItem.all
+    render json: @outfit_items
+  end
+
+  def show
+    @outfit_item = OutfitItem.find(params[:id])
+    render json: @outfit_item
+  end
+
+  def new
+    @outfit_item = OutfitItem.new
+  end
+
+  def create
+    @outfit_item = OutfitItem.find_or_create_by(outfititem_params)
+    render json: @outfit_item
+  end
+
+  def edit
+    @outfit_item = OutfitItem.find(params[:id])
+  end
+
+  def update
+    @outfit_item = OutfitItem.find(params[:id])
+    @outfit_item.update(outfit_item_params)
+    render json: @outfit_item.to_json
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:name)
+  end
+end
---
+++ app/controllers/outfits_controller.rb
@@ -0,0 +1,37 @@
+class OutfitsController < ApplicationController
+
+  def index
+    @outfits = Outfit.all
+    render json: @outfits
+  end
+
+  def show
+    @outfit = Outfit.find(params[:id])
+    render json: @outfit
+  end
+
+  def new
+    @outfit = Outfit.new
+  end
+
+  def create
+    @outfit = Outfit.find_or_create_by(outfit_params)
+    render json: @outfit
+  end
+
+  def edit
+    @outfit = Outfit.find(params[:id])
+  end
+
+  def update
+    @outfit = Outfit.find(params[:id])
+    @outfit.update(outfit_params)
+    render json: @outfit.to_json
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:name)
+  end
+end
