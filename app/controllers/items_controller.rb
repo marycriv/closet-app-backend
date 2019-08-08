18b3402af3ceb556b089b305611a16af9c472f25
marycriv <maryecrivelli@gmail.com>
2019-08-08 10:16:34 -0400

    Users and Items controllers created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,37 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    render json: @items
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    render json: @item
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.find_or_create_by(item_params)
+    render json: @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    render json: @item.to_json
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:user_id, :image, :classification, :brand)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_or_create_by(user_params)
+    render json: @user.to_json
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user = User.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :profile_picture)
+  end
+  
 end
