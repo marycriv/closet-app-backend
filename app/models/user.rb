a86c9e7d747e96b8ee85e3083a1cb9c27af30d39
marycriv <maryecrivelli@gmail.com>
2019-08-15 13:54:30 -0400

    Dependent destroy for followers added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :outfits
 
   validates :username, uniqueness: true
   validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 
-  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
-  has_many :followees, through: :followed_users
-  
-  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
-  has_many :followers, through: :following_users
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow', dependent: :destroy
+  has_many :followees, through: :followed_users, dependent: :destroy
+
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow', dependent: :destroy
+  has_many :followers, through: :following_users, dependent: :destroy
 
 end
